43cd3bd6566d35cbf5e0c764545fa4ca3d9ffe9b
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-02-08 14:08:00 +0100

    test that prints an error

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,21 @@ using Test
 @testset "ClearStacktrace.jl" begin
     # Write your own tests here.
 end
+
+module ModuleC
+    func_c() = error("Error")
+end
+
+
+module ModuleB
+    import ..ModuleC
+    func_b(::String, ::Rational) = ModuleC.func_c()
+end
+
+
+module ModuleA
+    import ..ModuleB
+    func_a(::Int) = ModuleB.func_b("a", 1//3)
+end
+
+ModuleA.func_a(1)
