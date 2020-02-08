72e91c503b89b25bef27329838e9bc0177e7490e
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-02-08 13:40:06 +0100

    more cleanup and settings

--- src/ClearStacktrace.jl
+++ src/ClearStacktrace.jl
@@ -2,7 +2,7 @@ module ClearStacktrace
 
 using Crayons
 
-const COLORS = Ref(Any[:red, :blue, :green, :yellow, :orange, :cyan, :magenta])
+const COLORS = Ref(Any[:blue, :yellow, :red, :green, :orange, :cyan, :magenta])
 const TYPECOLORS = Ref(Any[0x706060, 0x607060, 0x606080])
 const CRAYON_HEAD = Ref(Crayon(bold = true))
 const CRAYON_HEADSEP = Ref(Crayon(bold = true))
@@ -10,7 +10,9 @@ const CRAYON_FUNCTION = Ref(Crayon())
 const CRAYON_FUNC_EXT = Ref(Crayon(foreground = :dark_gray))
 const CRAYON_LOCATION = Ref(Crayon(foreground = :dark_gray))
 const CRAYON_NUMBER = Ref(Crayon(foreground = :blue))
-
+const NUMPAD = Ref(1)
+const FUNCPAD = Ref(2)
+const MODULEPAD = Ref(2)
 
 
 function expandbasepath(str)
@@ -29,74 +31,80 @@ function replaceuserpath(str)
 end
 
 getline(frame) = frame.line
-getfile(frame) = string(frame.file) |> expandbasepath
+getfile(frame) = string(frame.file) |> expandbasepath |> replaceuserpath
 getfunc(frame) = string(frame.func)
 getmodule(frame) = try; string(frame.linfo.def.module) catch; "" end
 
 getsig(frame) = try;  join("::" .* repr.(frame.linfo.specTypes.parameters[2:end]), ", ") catch; "" end
-getlocation(frame) = string(getfile(frame)) * ":" * string(getline(frame)) |> replaceuserpath
 
-function convert_frame(frame)
-    (
-        file = getfile(frame),
-        location = getlocation(frame),
-        func = getfunc(frame),
-        modul = getmodule(frame),
-        inlined = frame.inlined,
-        signature = getsig(frame),
-    )
+
+function convert_trace(trace)
+    files = getfile.(trace)
+    lines = getline.(trace)
+    funcs = getfunc.(trace)
+    moduls = getmodule.(trace)
+    signatures = getsig.(trace)
+    inlineds = getfield.(trace, :inlined)
+
+    # replace empty modules if there is another frame from the same file
+    for (i_this, mo) in enumerate(moduls)
+        if mo == ""
+            for i_other in 1:length(trace)
+                if files[i_this] == files[i_other] && moduls[i_other] != ""
+                    moduls[i_this] = moduls[i_other]
+                end
+            end
+        end
+    end
+
+    (files = files, lines = lines, funcs = funcs,
+        moduls = moduls, signatures = signatures, inlineds = inlineds)
 end
-
-convert_trace(trace) = convert_frame.(trace)
 
 
 
 function printtrace(io::IO, stacktrace)
 
-    arrframes = convert_trace(stacktrace)
+    files, lines, funcs, moduls, signatures, inlineds = convert_trace(stacktrace)
 
-    numbers = ["[" * string(i) * "]" for i in 1:length(arrframes)]
+    numbers = ["[" * string(i) * "]" for i in 1:length(stacktrace)]
     numwidth = maximum(length, numbers)
 
-    funcs = [x.func for x in arrframes]
-    exts = [x.inlined ? " [i]" : "" for x in arrframes]
+    exts = [inl ? " [i]" : "" for inl in inlineds]
     funcs_w_ext = funcs .* exts
     funcwidth = maximum(length, funcs_w_ext)
-
-    moduls = [x.modul for x in arrframes]
 
     modulwidth = max(maximum(length, moduls), length("Module"))
 
     umoduls = setdiff(unique(moduls), [""])
     ucolors = Dict(u => c for (u, c) in Iterators.zip(umoduls, COLORS[]))
 
-    locations = [x.location for x in arrframes]
+    # locations = [x.location for x in arrframes]
 
-    signatures = [x.signature for x in arrframes]
-
-    print(io, rpad("", numwidth + 1))
+    print(io, rpad("", numwidth + NUMPAD[]))
     # print(io, CRAYON_HEAD[](rpad("No.", numwidth + 1)))
-    print(io, CRAYON_HEAD[](rpad("Function", funcwidth + 2)))
-    print(io, CRAYON_HEAD[](rpad("Module", modulwidth + 2)))
+    print(io, CRAYON_HEAD[](rpad("Function", funcwidth + FUNCPAD[])))
+    print(io, CRAYON_HEAD[](rpad("Module", modulwidth + MODULEPAD[])))
     print(io, CRAYON_HEAD[]("Signature"))
     println(io)
     # print(io, CRAYON_HEAD[](rpad("┄┄┄", numwidth + 1)))
-    print(io, rpad("", numwidth + 1))
-    print(io, CRAYON_HEADSEP[](rpad("┄┄┄┄┄┄┄┄", funcwidth + 2)))
-    print(io, CRAYON_HEADSEP[](rpad("┄┄┄┄┄┄", modulwidth + 2)))
+    print(io, rpad("", numwidth + NUMPAD[]))
+    print(io, CRAYON_HEADSEP[](rpad("┄┄┄┄┄┄┄┄", funcwidth + FUNCPAD[])))
+    print(io, CRAYON_HEADSEP[](rpad("┄┄┄┄┄┄", modulwidth + MODULEPAD[])))
     print(io, CRAYON_HEADSEP[]("┄┄┄┄┄┄┄┄┄"))
     println(io)
 
-    for (i, (num, func, ext, modul, location, signature)) in enumerate(zip(numbers, funcs, exts, moduls, locations, signatures))
-        print(io, CRAYON_NUMBER(rpad(num, numwidth + 1)))
+    for (i, (num, func, ext, modul, file, line, signature)) in enumerate(
+            zip(numbers, funcs, exts, moduls, files, lines, signatures))
+        print(io, CRAYON_NUMBER[](rpad(num, numwidth + NUMPAD[])))
 
         print(io, CRAYON_FUNCTION[](func))
 
-        print(io, CRAYON_FUNC_EXT[](ext * (" " ^ (2 + funcwidth - length(funcs_w_ext[i])))))
+        print(io, CRAYON_FUNC_EXT[](ext * (" " ^ (FUNCPAD[] + funcwidth - length(funcs_w_ext[i])))))
 
         mcolor = get(ucolors, modul, :white)
         mcrayon = Crayon(foreground = mcolor)
-        print(io, mcrayon(rpad(modul, modulwidth + 2)))
+        print(io, mcrayon(rpad(modul, modulwidth + MODULEPAD[])))
 
         # sigcrayon = Crayon(foreground = :dark_gray)
         # print(io, sigcrayon(signature))
@@ -104,7 +112,7 @@ function printtrace(io::IO, stacktrace)
         
         println(io)
         # CRAYON_LOCATION = Crayon(foreground = 0x666666)
-        println(io, CRAYON_LOCATION[](location))
+        println(io, CRAYON_LOCATION[](string(file) * ":" * string(line)))
     end
 end
 
