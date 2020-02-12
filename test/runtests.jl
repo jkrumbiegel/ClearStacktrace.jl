690d03345ed4a25b21e6f1021c3e5d595e700014
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-02-12 15:32:53 +0100

    make test just print, not actually error

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,28 @@
 using ClearStacktrace
 using Test
 
-@testset "ClearStacktrace.jl" begin
-    # Write your own tests here.
-end
-
 module ModuleC
     func_c() = error("Error")
-end
+    end
 
 
-module ModuleB
+    module ModuleB
     import ..ModuleC
     func_b(::String, ::Rational) = ModuleC.func_c()
-end
+    end
 
 
-module ModuleA
+    module ModuleA
     import ..ModuleB
     func_a(::Int) = ModuleB.func_b("a", 1//3)
 end
 
-ModuleA.func_a(1)
+@testset "ClearStacktrace.jl" begin
+    try
+        ModuleA.func_a(1)
+    catch e
+        st = stacktrace(catch_backtrace())
+        Base.show_backtrace(stdout, st)
+    end
+end
+
